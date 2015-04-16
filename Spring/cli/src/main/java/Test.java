78a99555ecef0e05423e477f5dd8f4bcd050f348
Pavel Korshunov <halfofpoint@users.noreply.github.com>
2015-04-16 13:22:37 +0300

    Update Test.java

--- Spring/cli/src/main/java/Test.java
+++ Spring/cli/src/main/java/Test.java
@@ -1,2 +1,10 @@
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
 @Component
 public class Caller implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+      system.out.println("Everething bad");
+    }
+}
